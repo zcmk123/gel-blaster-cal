bef8265aa786d6e0e0208d47c8f71e0b6e2899ab
zcmk123 <zcmk123@users.noreply.github.com>
2020-08-09 15:35:30 +0000

    deploy: 08a899d1816fbc9ccabe8b9499be7aa201497285

---
+++ precache-manifest.48906597545fbd66a3ecfa11356b7df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a863d630f31e7de5c9d060407e802ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525b2ea01a0150291eb3",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "a8463b40fc9aba5b996b",
+    "url": "/static/css/main.56b34dc3.chunk.css"
+  },
+  {
+    "revision": "525b2ea01a0150291eb3",
+    "url": "/static/js/2.550c09aa.chunk.js"
+  },
+  {
+    "revision": "a8463b40fc9aba5b996b",
+    "url": "/static/js/main.63a14bf2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
