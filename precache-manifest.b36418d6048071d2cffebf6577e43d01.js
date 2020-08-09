e16c3358d49007dfdf0a2ce0e5358105c77860df
zcmk123 <zcmk123@users.noreply.github.com>
2020-08-09 15:49:21 +0000

    deploy: ae98ef7e2f27c574c82beb897b9f3289a5e843e8

---
+++ precache-manifest.b36418d6048071d2cffebf6577e43d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2616112fa4253f7e98ee1087a6db251a",
+    "url": "/gel-blaster-cal/index.html"
+  },
+  {
+    "revision": "ecd2e464018350226cda",
+    "url": "/gel-blaster-cal/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "a8463b40fc9aba5b996b",
+    "url": "/gel-blaster-cal/static/css/main.56b34dc3.chunk.css"
+  },
+  {
+    "revision": "ecd2e464018350226cda",
+    "url": "/gel-blaster-cal/static/js/2.52c9d191.chunk.js"
+  },
+  {
+    "revision": "a8463b40fc9aba5b996b",
+    "url": "/gel-blaster-cal/static/js/main.63a14bf2.chunk.js"
+  },
+  {
+    "revision": "90cb6b11348a56d08d08",
+    "url": "/gel-blaster-cal/static/js/runtime~main.c4e6a6aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48906597545fbd66a3ecfa11356b7df6.js"
+  "/gel-blaster-cal/precache-manifest.b36418d6048071d2cffebf6577e43d01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gel-blaster-cal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
