78736538aa4ccead148d90b5f77b63dc8be448a3
zcmk123 <zcmk123@users.noreply.github.com>
2020-08-09 15:38:15 +0000

    deploy: 07a4d46a31b3a5c406476339214b906c5179bc7e

--- precache-manifest.48906597545fbd66a3ecfa11356b7df6.js
+++ precache-manifest.48906597545fbd66a3ecfa11356b7df6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c135b284166733f14a156f9e28f75c97",
+    "revision": "0a863d630f31e7de5c9d060407e802ac",
     "url": "/index.html"
   },
   {
-    "revision": "f448ef5dd4a7521f7b31",
+    "revision": "525b2ea01a0150291eb3",
     "url": "/static/css/2.9c8c8187.chunk.css"
   },
   {
-    "revision": "b32e80f32e49ff7da369",
+    "revision": "a8463b40fc9aba5b996b",
     "url": "/static/css/main.56b34dc3.chunk.css"
   },
   {
-    "revision": "f448ef5dd4a7521f7b31",
-    "url": "/static/js/2.46865cd8.chunk.js"
+    "revision": "525b2ea01a0150291eb3",
+    "url": "/static/js/2.550c09aa.chunk.js"
   },
   {
-    "revision": "b32e80f32e49ff7da369",
-    "url": "/static/js/main.f0058a6f.chunk.js"
+    "revision": "a8463b40fc9aba5b996b",
+    "url": "/static/js/main.63a14bf2.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
